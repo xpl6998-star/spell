04cfbcdae74bb02c64220045c5d5bacc83550822
pengli xu <xpl6998@gmail.com>
2026-06-01 18:51:02 +0800

    Initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <>
+      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
+      <span>
+        <span>UpHotspot 2026</span>
+      </span>
+    </>
+  )
+}
